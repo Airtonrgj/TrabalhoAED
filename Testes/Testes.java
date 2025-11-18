c21135812434a5bbbc4e7ea8c35c7a4d149163a4
Airton Rodrigues <jarodriguesj1@gmail.com>
2025-11-18 20:30:54 -0300

    Revert "Preparando a busca no grafo e implimentando a lógica para criação de filas e listas"
    This reverts commit a1fd7144924f7e8c485b302b73e96ff4d65b4ee7.

--- Testes/Testes.java
+++ Testes/Testes.java
@@ -1,10 +1,2 @@
-import br.edu.icev.aed.forense.MinhaAnaliseForense;
-
-import java.io.IOException;
-
 public class Testes {
-    static void main(String[] args) throws IOException {
-        MinhaAnaliseForense testes = new MinhaAnaliseForense();
-        testes.encontrarSessoesInvalidas("");
-    }
 }
